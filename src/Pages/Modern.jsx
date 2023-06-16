c48fee19375ffcdd2889e9277de52b5f43176a74
Rajib-debnath1 <rajibdebnath519@gmail.com>
2023-06-16 23:54:30 +0600

    data fetch  added

--- src/Pages/Modern.jsx
+++ src/Pages/Modern.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 
 const Modern = () => {
+
+    const [data,setData] = useState([])
+
+    useEffect(() => {
+        // Perform the data fetch
+
+        axios.get(`${MainApi}/allinstructor`).then((data) => {
+            //   console.log(data.data);
+            setData(data.data);
+        });
+
+    }, []);
     return (
         <div>
             
